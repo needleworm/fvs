903d16f6f223436c23445daadb1016c81d0156b3
bhban <needleworm@kaist.ac.kr>
2017-03-10 22:12:02 +0900

    None Feedback Node Trimming

--- network.py
+++ network.py
@@ -15,6 +15,10 @@ class Network:
     def __init__(self, node_file, network_file):
         self.matrix = []
         self.nodes = []
+        self._update(node_file, network_file)
+        self._trim_none_feedback_nodes()
+
+    def _update(self, node_file, network_file):
         print("initiation.....")
         f_node = open("networks/" + node_file)
         f_network = open("networks/" + network_file)
@@ -39,6 +43,38 @@ class Network:
         f_node.close()
         f_network.close()
         print("Adjacency Matrix Done")
+
+    def _trim_none_feedback_nodes(self):
+        count = 0
+        while 1:
+            idx = self._is_there_none_feedback_node()
+            if idx < 0:
+                print("There are " + str(count) + " none-feedback node exist!")
+                if count > 0:
+                    print("All none-feedback nodes are removed from the network.")
+                return
+            self._remove_node_from_network(idx)
+            count += 1
+
+    def _remove_node_from_network(self, idx):
+        self.n -= 1
+        new_matrix = np.zeros((self.n, self.n), dtype='bool')
+        y = 0
+        for i in range(self.n + 1):
+            x = 0
+            for j in range(self.n + 1):
+                if i != idx and j != idx:
+                    new_matrix[y][x] = self.matrix[i][j]
+                    x += 1
+            if i != idx:
+                y += 1
+        self.matrix = new_matrix
+
+    def _is_there_none_feedback_node(self):
+        for i in range(self.n):
+            if np.sum(self.matrix[:, i]) == 0 or np.sum(self.matrix[i,:]) == 0:
+                return i
+        return -1
 
     def remove_nodes(self, combination, size):
         modified_matrix = np.array(self.matrix)
