18deb0eb6793e771c73b30f0f6090e33f74ad58c
Byunghyun Ban <needleworm@kaist.ac.kr>
2017-04-06 09:22:56 +0900

    recent

--- main.py
+++ main.py
@@ -4,8 +4,24 @@ import fvsFinder as fv
 #b = fv.FVSFinder("toy_annotation.txt")
 #c = fv.FVSFinder("Fumia_cytoscape.txt")
 
-e = fv.FVSFinder("test1.csv")
+#e = fv.FVSFinder("test1.csv")
 f = fv.FVSFinder("test2.csv")
 
-e = fv.FVSFinder("test1.csv", checker=True, fvs_found = ['FoxD-a/b'])
+#e = fv.FVSFinder("test1.csv", checker=True, fvs_found = ['FoxD-a/b'])
+"""aa = [['SOS', 'ErbB11', 'CaM', 'PL4/5-p2'],
+['SOS', 'ErbB11', 'CaMKII', 'PL4/5-p2'],
+['SOS', 'ErbB11', 'cytCa2+', 'DAG'],
+['SOS', 'ErbB11', 'cytCa2+', 'PKC'],
+['SOS', 'ErbB11', 'cytCa2+', 'phosphatidylacid'],
+['SOS', 'ErbB11', 'cytCa2+', 'PI5K'],
+['SOS', 'ErbB11', 'cytCa2+', 'PL4/5-p2'],
+['SOS', 'ErbB11', 'cytCa2+', 'PI4-P'],
+['SOS', 'ErbB11', 'cytCa2+', 'PLD'],
+['ErbB11', 'CaM', 'ERK1/2', 'PL4/5-p2'],
+['ErbB11', 'CaMKII', 'ERK1/2', 'PL4/5-p2'],
+['ErbB11', 'cytCa2+', 'ERK1/2', 'PL4/5-p2']]
 
+for i in aa:
+	g = fv.FVSFinder("test2.csv", checker=True, fvs_found=i)
+
+"""
